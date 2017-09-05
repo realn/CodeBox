0660e0e126c8f68f8a76d2d95a430df6c056e8b8
realn <realnoname@gmail.com>
2017-09-05 06:26:03 +0200

    CBStr, CBSDL: Corrections to ENUM_FLAG macro.

--- include/CBSDL/Defines.h
+++ include/CBSDL/Defines.h
@@ -31,7 +31,7 @@ namespace cb {
 
 #ifndef ENUM_FLAG
 #define ENUM_FLAG_OPERATOR(Type,Op) \
-static Type const operator Op(Type const& arg1, Type const& arg2) { \
+constexpr Type const operator Op(Type const& arg1, Type const& arg2) { \
   return static_cast<Type const>(static_cast<unsigned const>(arg1) Op static_cast<unsigned const>(arg2)); \
 };
 #define ENUM_FLAG(Type) \
@@ -39,7 +39,7 @@ enum class Type; \
 ENUM_FLAG_OPERATOR(Type, |) \
 ENUM_FLAG_OPERATOR(Type, &) \
 ENUM_FLAG_OPERATOR(Type, ^) \
-static Type const operator ~(Type const& arg) { \
+constexpr Type const operator ~(Type const& arg) { \
   return static_cast<Type const>(~static_cast<unsigned const>(arg)); \
 }; \
 enum class Type \
--- include/CBStr/Defines.h
+++ include/CBStr/Defines.h
@@ -102,4 +102,21 @@ namespace cb {
   }
 }
 
+#ifndef ENUM_FLAG
+#define ENUM_FLAG_OPERATOR(Type,Op) \
+constexpr Type const operator Op(Type const& arg1, Type const& arg2) { \
+  return static_cast<Type const>(static_cast<unsigned const>(arg1) Op static_cast<unsigned const>(arg2)); \
+};
+#define ENUM_FLAG(Type) \
+enum class Type; \
+ENUM_FLAG_OPERATOR(Type, |) \
+ENUM_FLAG_OPERATOR(Type, &) \
+ENUM_FLAG_OPERATOR(Type, ^) \
+constexpr Type const operator ~(Type const& arg) { \
+  return static_cast<Type const>(~static_cast<unsigned const>(arg)); \
+}; \
+enum class Type \
+
+#endif
+
 #endif // !__CB_STRING_DEFINES_H__
