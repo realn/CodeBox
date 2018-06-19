5a6b1cebac475f12f55cd39b0f193548d100b54e
realn <realnoname@coderulers.info>
2018-06-19 07:04:31 +0200

    Added missing scancode to/from string functions.

--- include/CBSDL/Funcs.h
+++ include/CBSDL/Funcs.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include "Defines.h"
+
 namespace cb {
   namespace sdl {
+    enum class ScanCode;
+
     extern bool IsCursorVisible();
     extern void SetCursorVisible(bool const value);
+
+    extern cb::string GetNameFromScanCode(ScanCode const code);
+    extern ScanCode GetScanCodeFromName(cb::string const& name);
   }
 }
--- src/CBSDL/Funcs.cpp
+++ src/CBSDL/Funcs.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "../../include/CBSDL/Funcs.h"
+#include "../../include/CBSDL/Consts.h"
 
 #include <SDL_mouse.h>
 
@@ -11,6 +12,19 @@ namespace cb {
 
     void SetCursorVisible(bool const value) {
       SDL_ShowCursor(value ? SDL_ENABLE : SDL_DISABLE);
+      CB_SDL_CHECKERRORS();
+    }
+
+    cb::string GetNameFromScanCode(ScanCode const code) {
+      auto szName = SDL_GetScancodeName(static_cast<SDL_Scancode>(code));
+      CB_SDL_CHECKERRORS();
+      return cb::fromUtf8(cb::utf8string(szName));
+    }
+
+    ScanCode GetScanCodeFromName(cb::string const & name) {
+      auto code = SDL_GetScancodeFromName(cb::toUtf8(name).c_str());
+      CB_SDL_CHECKERRORS();
+      return static_cast<cb::sdl::ScanCode>(code);
     }
   }
 }
